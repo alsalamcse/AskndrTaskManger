8bcebb6e89d30c503a3822499b30ab306e7e74c3
Owner <Askndr312@gmail.com>
2019-09-19 14:07:25 +0300

    icon

--- app/src/main/java/com/example/askndrtaskmanger/WelcomeScreen.java
+++ app/src/main/java/com/example/askndrtaskmanger/WelcomeScreen.java
@@ -2,6 +2,7 @@ package com.example.askndrtaskmanger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class WelcomeScreen extends AppCompatActivity {
@@ -10,5 +11,22 @@ public class WelcomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+
+        Thread th=new Thread()
+        {
+            @Override
+            public void run() {
+                try {
+                    sleep(3*1000);
+                    Intent i=new Intent(getApplication(),SigninScreen.class);
+                    startActivity(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        th.start();
+
     }
 }

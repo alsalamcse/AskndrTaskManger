9814db8588fb77483c60ed98fd93cb0bd5198c3e
Owner <Askndr312@gmail.com>
2019-09-22 13:58:46 +0300

    firebase

--- app/src/main/java/com/example/askndrtaskmanger/SigninScreen.java
+++ app/src/main/java/com/example/askndrtaskmanger/SigninScreen.java
@@ -3,6 +3,7 @@ package com.example.askndrtaskmanger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -20,5 +21,22 @@ public class SigninScreen extends AppCompatActivity {
         etPass=findViewById(R.id.etPass);
         btnSignin=findViewById(R.id.btnsSignin);
         btnSignup=findViewById(R.id.btnSignup);
+
+
+
+       btnSignin.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               dataHandler();
+           }
+       });
+
     }
-}
+    private void dataHandler()
+    {
+            String email=etemail.getText().toString();
+            String password=etPass.getText().toString();
+
+        }
+    }
+

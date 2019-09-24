bb57dff56715a5ef6910cd78abbca2812c0f568c
Owner <Askndr312@gmail.com>
2019-09-24 12:00:42 +0300

    Auth

---
+++ app/src/main/java/com/example/askndrtaskmanger/TempAllTasksACtivity.java
@@ -0,0 +1,14 @@
+package com.example.askndrtaskmanger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TempAllTasksACtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp_all_tasks_activity);
+    }
+}

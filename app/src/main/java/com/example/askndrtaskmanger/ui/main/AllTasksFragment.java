dbb5e02013b963427d08ae90212b45751dbe47d1
Owner <Askndr312@gmail.com>
2019-10-27 12:19:22 +0200

    WElcome

--- app/src/main/java/com/example/askndrtaskmanger/ui/main/AllTasksFragment.java
+++ app/src/main/java/com/example/askndrtaskmanger/ui/main/AllTasksFragment.java
@@ -3,16 +3,23 @@ package com.example.askndrtaskmanger.ui.main;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.askndrtaskmanger.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import Data.MyTask;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -37,6 +44,23 @@ public class AllTasksFragment extends Fragment {
         FirebaseAuth auth=FirebaseAuth.getInstance();
         String uid=auth.getUid();
         DatabaseReference reference=database.getReference();
+        reference.child("Tasks").child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                for (DataSnapshot d:dataSnapshot.getChildren())
+                {
+                    MyTask task=d.getValue(MyTask.class);
+                    Log.d("MyTask",task.toString());
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
 

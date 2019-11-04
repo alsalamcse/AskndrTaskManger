14da9b78f79fb0f368d300982716cb9e70663802
Owner <Askndr312@gmail.com>
2019-11-04 09:41:02 +0200

    Fragments

--- app/src/main/java/com/example/askndrtaskmanger/ui/main/AllTasksFragment.java
+++ app/src/main/java/com/example/askndrtaskmanger/ui/main/AllTasksFragment.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.askndrtaskmanger.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -20,11 +21,15 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import Data.MyTask;
+import Data.TaskAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AllTasksFragment extends Fragment {
+
+    private TaskAdapter taskAdapter;
+    private ListView lvTasks;
 
 
     public AllTasksFragment() {
@@ -35,8 +40,13 @@ public class AllTasksFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        taskAdapter=new TaskAdapter(getContext());
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_all_tasks, container, false);
+        View view = inflater.inflate(R.layout.fragment_all_tasks, container, false);
+        lvTasks=view.findViewById(R.id.lstvTasks);
+
+        return view;
 
     }
     public void readTasksFromFirebase(){

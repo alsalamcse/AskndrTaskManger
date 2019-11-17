76322342d711b616d612f7b329a98cf4288c412f
Owner <Askndr312@gmail.com>
2019-11-17 10:19:58 +0200

    Fragments

--- app/src/main/java/Data/TaskAdapter.java
+++ app/src/main/java/Data/TaskAdapter.java
@@ -10,6 +10,7 @@ import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -61,7 +62,11 @@ public class TaskAdapter extends ArrayAdapter<MyTask>
                         public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
                             if(databaseError==null)
                             {
+                                Toast.makeText(getContext(), "deleted", Toast.LENGTH_SHORT).show();
 
+                            }
+                            else {
+                                Toast.makeText(getContext(), "not deleted"+databaseError.getMessage(), Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
